2a33df88364b399fc920800592fe41e74cab8b21
yshan17 <han.lolove17@gmail.com>
2014-11-27 16:24:37 +0900

    Read data function

--- R/readData.R
+++ R/readData.R
@@ -6,7 +6,21 @@
 #' @param pattern Pattern of load data
 #' @return data file
 #' @export data file
+#' @examples 
+#' # Two csv files exists 'C:/User/USER/Desktop/data' path
+#' # One data is 'data1', the other is 'data2'
+#' 
+#' readData(data.name=c('data1','data2'), folder.path='C:/User/USER/Desktop/data', pattern='*.csv')
 readData <- function(data.name, folder.path, pattern='*.csv'){
+  
+  ## Description : 특정 경로에서 특정pattern을 가지는 파일을 모두 읽어들이는 함수
+  #
+  ## Arguments
+  # data.name : 저장될 데이터의 이름 지정 (path안의 파일 갯수와 같음)
+  # folder.path : 데이터를 불러올 폴더의 경로 
+  # pattern : 불러올 데이터의 패턴 지정
+  
+  require(stringr)
   
   cat('Loading file name...\n')
   data.files <- list.files(path=folder.path, pattern=pattern)
@@ -29,3 +43,4 @@ readData <- function(data.name, folder.path, pattern='*.csv'){
   }
   cat('Done!\n')
 }
+
