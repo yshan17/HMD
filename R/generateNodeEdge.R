3ac55bfa938a9d9d330d284f74f9f2555d751194
yshan17 <han.lolove17@gmail.com>
2014-12-04 15:23:10 +0900

    Remove assign() Replace return()

--- R/generateNodeEdge.R
+++ R/generateNodeEdge.R
@@ -8,43 +8,53 @@
 #' @param rules.minlen an integer value for the minimal number of items per item set (default : 2)
 #' @param rules.maxlen an integer value for the maximal number of items per item set (default : 2)
 #' 
-#' @return Node and Edge object(data.frame) from apriori function
+#' @return list object (First list is Node, second list is Edge.) from apriori function
 #' @examples
 #' # Set support value 0.01
-#' generateNE(HMDM.data=data.freq, rules.supp=0.01)
-generateNE <- function(HMDM.data, rules.supp=0.1, rules.conf=0.8, rules.minlen=2, rules.maxlen=2){
+#' NodeEdge <- generateNodeEdge(sony.freq)
+#' NodeEdge[[1]]  # Node
+#' NodeEdge[[2]]  # Edge
+
+generateNE <- function(HMDM.data, rules.minlen, rules.maxlen ,rules.supp=0.1, rules.conf=0.8){
   
   require(arules)
+  require(stringr)
   
   freq.data.m <- as.matrix(HMDM.data)
   row.names(freq.data.m) <- 1:nrow(HMDM.data)
   
-  # Association : apriori function in 'arules' package
   transaction_m <- as(freq.data.m, 'transactions')
+  # summary(transaction_m)
   rules.all <- apriori(transaction_m, parameter=list(minlen=rules.minlen, maxlen=rules.maxlen, supp=rules.supp, conf=rules.conf))
   # inspect(rules.all)
   
-  # Network Graph
-  
-  ## 1. Edge - 1
-  # Source
+  #### Edge ####
+  #Source
   lhs <- as(rules.all@lhs, "list")
   lhs.s <- as.character(lhs) 
-  # Target
+  
+  #Target
   rhs <- as(rules.all@rhs, "list")
   rhs.t <- as.character(rhs) 
   
-  ## 2. Node
-  # label
+  #Support
+  sup <- (rules.all@quality)$support
+  
+  #Confidence
+  con <- (rules.all@quality)$confidence
+  
+  #### Node ####
   label.tmp1 <- unique(lhs.s)
   label.tmp2 <- unique(rhs.t)
   label <- unique(c(label.tmp1, label.tmp2))
-  # id
+  
   id <- as.character(c(1:length(label)))
+  
   Node <- data.frame(id=id, label=label, stringsAsFactors=FALSE)
   
-  # Freq of Node
+  # Node Freq
   wordfreq <- rowSums(t(freq.data.m))
+  names(wordfreq)
   word.freq <- data.frame(label=names(wordfreq), freq=wordfreq)
   Node.freq <- merge(word.freq, Node, by='label')
   Node.freq <- Node.freq[,3:2]
@@ -52,18 +62,23 @@ generateNE <- function(HMDM.data, rules.supp=0.1, rules.conf=0.8, rules.minlen=2
   Node.freq <- Node.freq[ order(Node.freq$id),]
   Node.freq$id <- as.character(Node.freq$id)
   Node$freq <- Node.freq$freq
+  Node$level1 <- unlist(lapply(1:nrow(Node), function(x) (str_split(Node$label, pattern='-'))[[x]][1]))
   
-  ## 3. Edge - 2
+  #### Edge ####
   
-  Edge <- data.frame(source=lhs.s, label=rhs.t, stringsAsFactors=FALSE)
+  Edge <- data.frame(source=lhs.s, label=rhs.t, supp=sup, conf=con, stringsAsFactors=FALSE)
   
-  Edge.1 <- merge(Edge, Node, by="label")
-  Edge.2 <- Edge.1[,2:3]
-  colnames(Edge.2) <- c("label", "target")
-  Edge.3 <- merge(Edge.2, Node, by="label")
-  Edge.4 <- Edge.3[,3:2]
-  colnames(Edge.4) <- c("source", "target")
+  Edge.1 <- merge(Edge, Node[,1:2], by="label")
+  Edge.2 <- Edge.1[,2:5]
+  colnames(Edge.2) <- c("label", 'supp','conf', "target")
+  Edge.3 <- merge(Edge.2, Node[,1:2], by="label")
+  Edge.4 <- Edge.3[,c(5,4,2,3)]
+  colnames(Edge.4) <- c("source", "target",'supp','conf') # head(Edge.4)
   
   assign('Node', Node, envir=.GlobalEnv)
   assign('Edge', Edge.4, envir=.GlobalEnv)
+  
+  return(list(Node=Node, Edge=Edge.4))
+  
 }
+
